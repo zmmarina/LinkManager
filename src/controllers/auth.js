e09fb0559268b35937b799ccb21898215950be71
ZMarina Monteiro <zmarina.hm@gmail.com>
2020-07-02 06:08:26 -0300

    storing accounts into db

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,12 +1,25 @@
 const express = require ("express");
+const bcrypt = require ("bcrypt");
+const { Account } = require ("../models");
+
 const router = express.Router();
 
-router.get("/sign-in", (req, res) =>{
+const saltRounds = 10;
+
+
+router.get("/sign-in", (req, res) =>{    
     return res.json("Sign in");
 });
 
-router.get("/sign-up", (req, res) =>{
-    return res.json("Sign up");
+
+router.get("/sign-up", async (req, res) =>{
+    const email = "zmarina@gmail.com";
+    const password = "123456";
+
+    const hash = bcrypt.hashSync(password, saltRounds);
+    const result = await Account.create({email, password: hash });
+    
+    return res.json(result);
 });
 
 
