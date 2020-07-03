5a08f26e6f86b401dcf55c2f2ec6ddbc170131cf
ZMarina Monteiro <zmarina.hm@gmail.com>
2020-07-03 18:14:41 -0300

    jwt implemented

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,20 +1,31 @@
 const express = require ("express");
 const bcrypt = require ("bcrypt");
 const { Account } = require ("../models");
-const { accountSignUp } = require ("../validators/account");
+const { accountSignUp, accountSignIn } = require ("../validators/account");
 const { getMessage } = require ("../helpers/messages");
+const { generateJwt, generateRefreshJwt } = require ("../helpers/jwt");
 
 const router = express.Router();
 
 const saltRounds = 10;
 
 
-router.get("/sign-in", (req, res) =>{    
-    return res.jsonOK(null);
+router.post("/sign-in", accountSignIn, async (req, res) =>{    
+
+    const {email, password} = req.body;
+
+    const account = await Account.findOne({ where: { email } });
+    const match = account ? bcrypt.compareSync(password, account.password) : null;
+    if (!match) return res.jsonBadRequest(null, getMessage("account.signin.invalid"));
+    
+    const token = generateJwt({id: account.id});
+    const refreshToken = generateRefreshJwt({id: account.id});
+        
+    return res.jsonOK(account,  getMessage("account.signin.success"), { token, refreshToken });   
 });
 
 
-router.get("/sign-up", accountSignUp, async (req, res) =>{
+router.post("/sign-up", accountSignUp, async (req, res) =>{
 
     const {email, password} = req.body;
 
@@ -24,8 +35,12 @@ router.get("/sign-up", accountSignUp, async (req, res) =>{
 
     const hash = bcrypt.hashSync(password, saltRounds);
     const newAccount = await Account.create({email, password: hash });
+
+    const token = generateJwt({id: newAccount.id});
+    const refreshToken = generateRefreshJwt({id: newAccount.id});
     
-    return res.jsonOK(newAccount, getMessage("account.signup.success"));
+    
+    return res.jsonOK(newAccount, getMessage("account.signup.success"), { token, refreshToken });
 });
 
 
