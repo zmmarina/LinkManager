48317de12c4e7beffcceffd3ed49add6d712cc0f
ZMarina Monteiro <zmarina.hm@gmail.com>
2020-07-04 03:58:38 -0300

    excluded auth paths from JWT

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -3,21 +3,23 @@ const { verify } = require("jsonwebtoken");
 
 const checkJwt = (req, res, next) =>{
 
-   let token =  req.headers["authorization"];
+    const { url : path} = req;
+    const excludedPaths = ["/auth/sign-in", "/auth/sign-up"];
+    const isExcluded = !!excludedPaths.find(p => p.startsWith(path));
+    if (isExcluded) return next();
 
+   let token =  req.headers["authorization"];
    token = token ? token.slice(7, token.length) : null;
    if (!token){
        return res.jsonUnauthorized(null, "Invalid token");
    }
 
    try{
-
     const decoded = verifyJwt(token);
     req.accountId = decoded.id;
     next();
 
    }catch(error){
-
     return res.jsonUnauthorized(null, "Invalid token");    
    }
 };
